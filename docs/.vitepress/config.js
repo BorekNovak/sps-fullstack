6f6c9bda7ecd5534cf5935ca337409b58bc88e56
Borek Novak <borek.novak@student.sps-cl.cz>
2025-06-09 10:28:54 +0200

    feat: added documentation

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,20 @@
+export default {
+  title: "Správa parkovacích míst",
+  description: "Dokumentace k projektu parkování v garáži",
+  themeConfig: {
+    nav: [
+      { text: 'Úvod', link: '/' },
+      { text: 'Popis projektu', link: '/project' },
+      { text: 'Struktura', link: '/structure' },
+      { text: 'API', link: '/api' },
+      { text: 'Spuštění', link: '/run' }
+    ],
+    sidebar: [
+      { text: 'Úvod', link: '/' },
+      { text: 'Popis projektu', link: '/project' },
+      { text: 'Struktura projektu', link: '/structure' },
+      { text: 'API dokumentace', link: '/api' },
+      { text: 'Návod na spuštění', link: '/run' }
+    ]
+  }
+}
